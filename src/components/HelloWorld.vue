66b3e1690811c652b4daf44a456f246c241b4dde
QianMoth <QianMoth@qq.com>
2023-02-27 13:00:37 +0800

    修改slogenyanse为黑色

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{ msg: string }>()
-
-const hue = ref(Math.random() * 360)
-
-function onMousemove(e: any) {
-  hue.value = e.clientX
-}
-
-function touchmove(e: any) {
-  hue.value = e.touches[0].clientX
-}
 </script>
 
 <template>
-  <h1
-    @mousemove="onMousemove"
-    @touchmove="touchmove"
-    :style="{ color: `hsl(${hue}, 80%, 50%)` }"
-    class="movearea"
-  >
+  <h1>
     {{ msg }}
   </h1>
 </template>
 
-<style scoped>
-.movearea {
-  transition: 0.3s color ease;
-}
-</style>
+<style scoped></style>
