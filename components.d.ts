137a9fecfbf3b7b3581aa40906454105af9b4d7d
zmoth <QianMoth@qq.com>
2023-03-01 21:24:40 +0800

    取消pinia、router依赖

--- components.d.ts
+++ components.d.ts
@@ -14,7 +14,5 @@ declare module '@vue/runtime-core' {
     ElTag: typeof import('element-plus/es')['ElTag']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
   }
 }
