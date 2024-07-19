78378e4660eae152dc9200f31cd2c2525c6f562c
zmoth <QianMoth@qq.com>
2024-07-19 23:49:45 +0800

    fix: `exclude` option is deprecated

--- uno.config.js
+++ uno.config.js
@@ -2,18 +2,22 @@
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
-  exclude: [
-    'node_modules',
-    '.git',
-    '.github',
-    '.husky',
-    '.vscode',
-    'build',
-    'dist',
-    'mock',
-    'public',
-    './stats.html',
-  ],
+  content: {
+    pipeline: {
+      exclude: [
+        'node_modules',
+        '.git',
+        '.github',
+        '.husky',
+        '.vscode',
+        'build',
+        'dist',
+        'mock',
+        'public',
+        './stats.html',
+      ],
+    },
+  },
   presets: [presetUno(), presetAttributify()],
   theme: {
     colors: {
